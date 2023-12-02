c9b58c6d1916bee9fca086a63bf8e64a4e70d3f9
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2023-12-01 19:53:29 -0500

    yet again

--- src/componets/Hero/Calculator.jsx
+++ src/componets/Hero/Calculator.jsx
@@ -1,38 +1,47 @@
 import React from "react";
-import classes from './Calculator.module.css'
+import classes from "./Calculator.module.css";
 
 export default function Calculator() {
   return (
     <div className={classes.container}>
-      
-        <div className={classes.title}>
-          <h3>Enter your details below</h3>
+      <div className={classes.title}>
+        <h3>Enter your details below</h3>
+      </div>
+      <div className={classes.metric}>
+        <div className={classes.radio1}>
+          <input type="radio" id="metric" value="metric" name="metric" />
+          <label>Metric</label>
         </div>
-        <div className={classes.metric}>
-          <label>
-            <input type="radio" id="metric" value="metric" /> Metric
-          </label>
-          <label>
-            <input type="radio" id="metric" value="metric" /> Imperial
-          </label>
+        <div className={classes.radio2}>
+          <input type="radio" id="metric" value="metric" name="metric" />
+          <label>Imperial</label>
         </div>
-        <div className={classes.inputs}>
-          <div className="height">
-            <label>Height</label>
+      </div>
+      <div className={classes.inputs}>
+        <div className={classes.height}>
+          <label>Height</label>
+          <div className={classes.heightInput}>
             <input />
-          </div>
-          <div className="weight">
-            <label>Weight</label>
-            <input />
-          </div>
-          <div className="result">
-            <div className="bmi-title">
-              <h2>Welcome</h2>
-              <p>Enter your height and weight and you'll see your BMI result here</p>
-            </div>
-            <div className="bmi-result"></div>
+            <h3>cm</h3>
           </div>
         </div>
+        <div className={classes.weight}>
+          <label>Weight</label>
+          <div className={classes.weightInput}>
+          <input />
+          <h3>kg</h3>
+          </div>
+          
+        </div>
+      </div>
+      <div className="result">
+        <div className="bmi-title">
+          <h2>Welcome</h2>
+          <p>
+            Enter your height and weight and you'll see your BMI result here
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
